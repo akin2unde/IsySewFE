4573a910eeef194aa74c11e5b18c9c2efb0ba5e1
DevTee4 <toyinayodele2012@gmail.com>
2021-08-12 20:02:37 +0100

    fix

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/Product';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products=
+  [
+     {
+      name:"Sally Fanny pack",
+      code:"Y523201",
+      description:"",
+      images:[{url:"https://upload.wikimedia.org/wikipedia/commons/a/a8/Derbys_Peter_Pan_peanut_butter_sample_blikje%2C_foto3.JPG"}],
+      price:50432,
+      categories:[],
+    },
+    {
+      name:"Juice Juicy",
+      code:"Y623201",
+      description:"",
+      images:[{url:"https://greendroprecycling.com/wp-content/uploads/2017/04/GreenDrop_Station_Aluminum_Can_1.jpg"}],
+      price:70432,
+      categories:[],
+    },
+    {
+      name:"Sally Fanny pack",
+      code:"Y523201",
+      description:"",
+      images:[{url:"https://identity-mag.com/wp-content/uploads/2017/07/cover.jpg"}],
+      price:80432,
+      categories:[],
+    },
+    {
+      name:"Beauty Product",
+      code:"Y523201",
+      description:"",
+      images:[{url:"https://assets.entrepreneur.com/content/3x2/2000/1396394917-10-innovative-beauty-products-disrupt-morning-beauty-routine.jpg"}],
+      price:90432,
+      categories:[],
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
